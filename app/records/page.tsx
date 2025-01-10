94ef481df8821884b0b2abe567ce3aebec8ab4be
HARISH130505 <harish13.05.05@gmail.com>
2025-01-10 16:42:06 +0530

    initial commit from harish

---
+++ app/records/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       <div className='p-6'>
+           <h1 className='font-poppins text-3xl'>Dashboard</h1>
+       </div>
+    </div>
+  )
+}
+
+export default page

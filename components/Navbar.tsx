b8be60071b698eb122e64cfb939c8f1926020b26
HARISH130505 <harish13.05.05@gmail.com>
2025-01-10 17:33:28 +0530

    new commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { useState } from "react";
+import Image from "next/image";
 import { Menu } from "lucide-react";
 import { SignedOut, SignInButton } from "@clerk/nextjs";
+import logo from "../public/logo.png"
 const Navbar = () => {
     const [menuFlg, setMenuFlg] = useState(false);
     function handleChange() {
@@ -9,8 +11,8 @@ const Navbar = () => {
     }
     return (
         <div>
-            <nav className="flex justify-between items-center bg-slate-800 text-slate-400">
-                <h1 className="p-4 xs:text-4xl">MedFolio</h1>
+            <nav className="flex justify-between items-center bg-slate-800 text-slate-400 py-3">
+                <Image src={logo} alt="image" width={200}/>
                 <ul className="hidden md:flex space-x-8 text-xl px-3">
                     <li>Home</li>
                     <li>Dashboard</li>
@@ -36,8 +38,10 @@ const Navbar = () => {
                             </button>
                             <ul className="bg-slate-800 w-full absolute top-16 left-0 text-2xl text-center">
                                 <li className="py-4">Home</li>
-                                <li className="py-4">About</li>
-                                <li className="py-4">Contact</li>
+                                <li className="py-4">Dashboard</li>
+                                <SignedOut>
+                                    <SignInButton/>
+                                </SignedOut>
                             </ul>
                         </>
                     )}

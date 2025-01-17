afbf3db0bd3e6cb4443dfaaed412522ce57d6a58
Harish <159623429+HARISH130505@users.noreply.github.com>
2025-01-17 21:41:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ const Page = () => {
     return (
      <div className="flex justify-center items-center text-white">
         <div className="flex flex-col justify-center items-center">
-           <div className="shadow-lg shadow-sky-800 w-[500] mt-12">
+           <div className="w-[250] shadow-lg shadow-sky-800 mt-12 md:w-[500] ">
               <Image src={ecg} alt="ecg" width={600}></Image>
            </div>
            <h1 className="font-comforta  text-3xl p-4">Med<span className="text-cyan font-comforta text-3xl">Folio</span></h1>
-           <p className="font-poppins text-xl">Revolutionizing medical portfolios with cutting-edge technology.</p>
+           <p className="text-center font-poppins md:text-xl">Revolutionizing medical portfolios with cutting-edge technology.</p>
            <div className="flex space-x-4 items-center mt-5">
             <Link href="/about">
                 <button className="flex justify-around  bg-cyan text-black font-poppins rounded-md p-3 w-52 ">Get Started<MoveRight/></button>

db9d4effea60a79068502a09281a7e13717cd40e
HARISH130505 <harish13.05.05@gmail.com>
2025-02-16 20:57:33 +0530

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,37 @@
 import React from "react";
-import ecg from "../public/ecg.gif"
+import ecg from "../public/ecg.gif";
 import Image from "next/image";
 import { Cross, MoveRight, Shield, Stethoscope } from "lucide-react";
 import Link from "next/link";
 
 const Page = () => {
-    return (
-     <div className="flex justify-center items-center text-white">
-        <div className="flex flex-col justify-center items-center">
-           <div className="w-[250] shadow-lg shadow-sky-800 mt-12 md:w-[500] ">
-              <Image src={ecg} alt="ecg" width={600}></Image>
-           </div>
-           <h1 className="font-comforta  text-3xl p-4">Med<span className="text-cyan font-comforta text-3xl">Folio</span></h1>
-           <p className="text-center font-poppins md:text-xl">Revolutionizing medical portfolios with cutting-edge technology.</p>
-           <div className="flex space-x-4 items-center mt-5">
-            <Link href="/about">
-                <button className="flex justify-around  bg-cyan text-black font-poppins rounded-md p-3 w-52 ">Get Started<MoveRight/></button>
-            </Link>
-            <Stethoscope className="text-cyan"/>
-            <Shield className="text-violet-400"/>
-            <Cross className="text-cyan"/>
-           </div>
+  return (
+    <div className="flex justify-center items-center text-white my-20">
+      <div className="flex flex-col justify-center items-center w-full px-4 md:px-0">
+        <div className="w-full shadow-lg shadow-sky-800 md:w-[500px]">
+          <Image src={ecg} alt="ecg" width={600} height={400} layout="responsive" objectFit="contain" />
         </div>
-     </div>
-    );
+        <h1 className="font-comforta text-3xl p-4 text-center">
+          Med<span className="text-cyan font-comforta text-3xl">Folio</span>
+        </h1>
+        <p className="text-center font-poppins md:text-xl px-4">
+          Revolutionizing medical portfolios with cutting-edge technology.
+        </p>
+        <div className="flex flex-col md:flex-row space-x-0 md:space-x-4 items-center mt-5 w-full justify-center">
+          <Link href="/about">
+            <button className="flex justify-around bg-cyan text-black font-poppins rounded-md p-3 w-full md:w-52 mb-4 md:mb-0">
+              Get Started<MoveRight />
+            </button>
+          </Link>
+          <div className="flex space-x-4">
+            <Stethoscope className="text-cyan" />
+            <Shield className="text-violet-400" />
+            <Cross className="text-cyan" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
